619bf6c0e2f09b10b1904b5c29deeac40b69a962
Adam Morris <amorris@mistermorris.com>
2013-04-19 09:39:35 +0800

    proper command line, settings_file argument

--- utils/ArgsParser.py
+++ utils/ArgsParser.py
@@ -3,13 +3,19 @@ class HoldPassedArugments:
     Class that uses the argsparse module and puts data into the passed object
     """
 
-    def __init__(self, switches):
+    def __init__(self, *switches, **strings):
+        """
+        switches are used for boolean values
+        strings are usef for passed values
+        """
         import argparse
         import sys
 
         parser = argparse.ArgumentParser()
         for switch in switches:
             parser.add_argument('--' + switch, action='store_true')
+        for string in strings.keys():
+            parser.add_argument('-' + string, action='store', dest=string, default=strings[string])
         self.arguments = parser.parse_args(sys.argv[1:])
         #for key, value in vars(parser.parse_args(sys.argv[1:])).items():
         #    setattr(self.arguments, key, value)
