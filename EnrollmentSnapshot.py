d4d2f3c4eaf890e9c54b8785046c10d8478d41f6
Adam Morris <amorris@mistermorris.com>
2013-04-29 19:56:22 +0800

    more data for enrollment

--- EnrollmentSnapshot.py
+++ EnrollmentSnapshot.py
@@ -42,13 +42,27 @@ class Breakdowns:
         self._db = {}
 
     def add(self, student):
-        target_grade = "grade_{grade}".format(**student.__dict__)
+        target = '   all'
+        secondary = '  secondary'
+        elementary = '  elementary'
+        target_grade = " grade_{grade}".format(**student.__dict__)
         target_hr    = "grade_{homeroom}".format(**student.__dict__)
+        if not target in self._db.keys():
+            self._db[target] = Breakdown('Overall')
         if not target_grade in self._db.keys():
             self._db[target_grade] = Breakdown('Grade {}'.format(student.grade))
         if not target_hr in self._db.keys():
             self._db[target_hr] = Breakdown('Homeroom {}'.format(student.homeroom))
+        if not secondary in self._db.keys():
+            self._db[secondary] = Breakdown('Secondary')
+        if not elementary in self._db.keys():
+            self._db[elementary] = Breakdown('Elementary')
 
+        self._db[target].add(student)
+        if student.is_secondary:
+            self._db[secondary].add(student)
+        if student.is_elementary:
+            self._db[elementary].add(student)
         self._db[target_grade].add(student)
         self._db[target_hr].add(student)
 
@@ -56,7 +70,7 @@ class Breakdowns:
         keys = list(self._db.keys())
         keys.sort()
         for key in keys:
-            print("In {identity} there are:\n\t{number_koreans} koreans ({percent_koreans})\n\t{number_chinese} chinese ({percent_chinese})\n\t{number_other} other ({percent_other})".format(**self._db[key].__dict__))
+            print("In {identity} there are:\n\t{number_koreans} koreans ({percent_koreans})\n\t{number_chinese} chinese ({percent_chinese})\n\t{number_other} other ({percent_other})\n\t---\n\t{_total} total".format(**self._db[key].__dict__))
 
 if __name__ == "__main__":
 
