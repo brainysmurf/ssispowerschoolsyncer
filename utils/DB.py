712abaf1de1db49de519fd151986c9d4f6fb357f
Adam Morris <amorris@mistermorris.com>
2013-05-08 14:54:21 +0800

    actually_sync feature

--- utils/DB.py
+++ utils/DB.py
@@ -340,6 +340,7 @@ class ServerInfo(DragonNetDBConnection):
             password = moodle_config.get('database_password')
             database = moodle_config.get('database_name')
             self.server = moodle_config.get('host')
+            self.actually_sync = moodle_config.get('sync', False)
 
             super().__init__(user, password, self.server, database, verbose=verbose)
             self.init_users_and_groups()
@@ -426,6 +427,8 @@ class ServerInfo(DragonNetDBConnection):
         Does not handle infinite loops though
         """
         if not self.db:
+            return
+        if not self.actually_sync:
             return
         idnumber = student.num
         username = student.username
