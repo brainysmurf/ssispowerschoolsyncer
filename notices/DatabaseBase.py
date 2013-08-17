92cd1c0a13ded260bf00325e51a1e8d3cf796da7
Adam Morris <amorris@mistermorris.com>
2013-08-17 12:46:21 +0800

    try notices again 6

--- notices/DatabaseBase.py
+++ notices/DatabaseBase.py
@@ -59,11 +59,11 @@ class ExtendMoodleDatabaseToAutoEmailer:
         # Setup formatting templates for emails, can be overridden if different look required
         # The default below creates a simple list format
         # Need two {{ and }} because it goes through a parser later at another layer
-        self.start_html_tag    = '<html><head><style>.local-alert {{margin-left:150px;margin-right:150px;margin-bottom:10px;padding: 15px 20px 15px 45px; background-color: #fff; border-top: 2px solid #4D63A3; border-bottom: 2px solid #4D63A3;}}</style></head><body bgcolor=#eee>'
+        self.start_html_tag    = '<html><head><style>.heading {{ font-family:"Tahoma",sans-serif;font-weight:bold;font-size:1.5em;}}.local-alert {{font-family:"Tahoma",sans-serif;margin-left:50px;margin-right:50px;margin-bottom:10px;padding: 15px 20px 15px 45px; background-color: #fff; border-top: 2px solid #4D63A3; border-bottom: 2px solid #4D63A3;}}</style></head><body bgcolor=#eee>'
         self.end_html_tag      = "</body></html>"
         self.end_html_tag      = "</html>"
-        self.header_pre_tag    = "<h3>"
-        self.header_post_tag   = "</h3>"
+        self.header_pre_tag    = '<div class="heading">'
+        self.header_post_tag   = "</div>"
         self.begin_section_tag = ""
         self.end_section_tag   = "<br />"
         self.begin_list_tag    = '<div class="local-alert">'
