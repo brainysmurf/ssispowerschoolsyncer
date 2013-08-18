0a3bb29f9554f4d221bdda9b38c231f09ad94aea
Adam Morris <amorris@mistermorris.com>
2013-08-19 07:11:10 +0800

    please work now 3

--- phpmoodle/phpclimoodle.php
+++ phpmoodle/phpclimoodle.php
@@ -51,7 +51,7 @@ class moodlephp
 	  return "-1 Could not find user ".$idnumber." while adding cohort ".$cohortidnumber;
 	}
 
-      if( ! ($cohort = $DB->get_record_select( 'cohort', "idnumber = '?'", array(0=>$cohortidnumber) )) ) {
+      if( ! ($cohort = $DB->get_record_select( 'cohort', "idnumber = '?'", array(1=>$cohortidnumber) )) ) {
 	//$cohortID = $this->create_cohort($idnumber, $cohortidnumber, '');
 	return "-1 Cohort does not exit";
       }
