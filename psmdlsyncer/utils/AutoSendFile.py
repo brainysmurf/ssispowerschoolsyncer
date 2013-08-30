117cd3ea3ccf81f42d281dbb5b07aa7e057abaaf
Adam Morris <amorris@mistermorris.com>
2013-08-30 15:10:09 +0800

    AutoSendFile now dynamically loads the latest version

--- psmdlsyncer/utils/AutoSendFile.py
+++ psmdlsyncer/utils/AutoSendFile.py
@@ -1,9 +1,30 @@
 import re
+import os
+
+# This is the version of the autosend files
+# With each increase of number in the version, means that something about the fields have changed from within autosend
+major_version = 3
+version_format = "{prefix}_{{school}}_{{unique}}_v{major_version}.{{minor_version}}".format(prefix='ssis', major_version=major_version)
+
+from psmdlsyncer.settings import config_get_section_attribute
 
 class File:
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, school, unique):
+        """
+        Main task here is to set self.path
+        """
+        path_to_powerschool = config_get_section_attribute('DIRECTORIES', 'path_to_powerschool_dump')
+        if not path_to_powerschool:
+            raise NotImplemented("Something wrong with the powerschool directory information")
+
+        # Dynamically look at the directory and use the latest version
+        path = path_to_powerschool + '/' + version_format.format(minor_version='', **locals())
+        path = path.split(os.path.sep)[-1]
+        candidates = [g for g in [f.split(os.path.sep)[-1] for f in os.listdir(path_to_powerschool)] if path in g]
+        candidates = sorted(candidates)
+        final = candidates[-1]
+        self.path = path_to_powerschool + '/' + final
         self.readin()
 
     def readin(self):
@@ -19,7 +40,7 @@ class File:
 
 if __name__ == "__main__":
 
-    f = File('../../powerschool/ssis_studentinfodumpall')
+    f = File('sec', 'studentschedule')
     for line in f.content():
         print(line)
     
