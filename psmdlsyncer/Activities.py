c0208129e54e037690a9d8076a8d440851f5d748
Adam Morris <amorris@mistermorris.com>
2013-09-04 10:07:29 +0800

    typo

--- psmdlsyncer/Activities.py
+++ psmdlsyncer/Activities.py
@@ -68,19 +68,20 @@ if __name__ == "__main__":
 
     # DO THE ACTIVITY EMAILS
     sf.path = config_get_section_attribute('DIRECTORIES', 'path_to_postfix')
-    sf.activities_path = sf('{path}/activities')
+    sf.base = 'activities'
     sf.SUFFIX = "ACT"
     sf.SPACE = ' '
     sf.EXT = '.txt'
     sf.NEWLINE = '\n'
     sf.SLASH = '/'  # uppercase for readability
     sf.INCLUDE = ':include:'
+    sf.activities_path = sf('{path}{SLASH}{base}')
     sf.space = ' '
     clear_folder(sf.activities_path)
     for activity_name in postfix:
         sf.handle = name_to_email(activity_name)
         sf.full_email = sf('{handle}{SUFFIX}')
-        with open(sf('{path}{EXT}'), 'a') as f:
+        with open(sf('{path}{SLASH}{base}{EXT}'), 'a') as f:
             f.write(sf('{full_email}{SPACE}{INCLUDE}{activities_path}{full_email}{EXT}{NEWLINE}'))
         with open(sf('{activities_path}{SLASH}{full_email}{EXT}'), 'a') as f:
             f.write("\n".join(postfix[activity_name]))
