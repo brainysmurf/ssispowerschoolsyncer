df3eb009641fc89386f437835ea1477a14424bac
Adam Morris <amorris@mistermorris.com>
2013-09-11 10:33:50 +0800

    DeleteAccounts.py

---
+++ psmdlsyncer/DeleteAccounts.py
@@ -0,0 +1,24 @@
+from psmdlsyncer.Students import Students, put_in_order
+from psmdlsyncer.settings import config
+from psmdlsyncer.utils.DB import DragonNetDBConnection
+from psmdlsyncer.utils.Formatter import Smartformatter
+from collections import defaultdict
+
+if __name__ == "__main__":
+
+    students = Students()
+    dnet = DragonNetDBConnection()
+    all_ids = dnet.get_all_students_name_ids()
+    to_delete = []
+
+    for this_id, first, last in all_ids:
+        if not this_id:
+            continue
+        student = students.get_student(this_id)
+        if not student:
+            if not students.get_teacher(this_id):
+                if not students.get_teacher(last + ', ' + first):
+                    to_delete.append( (this_id, last + ', ' + first) )
+
+    for this_id, username in to_delete:
+        print(this_id, username)
