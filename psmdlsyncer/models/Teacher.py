dde3c9fbcaf6a56f80b69ff343775c9e028acc23
brainysmurf <amorris@mistermorris.com>
2013-10-05 10:23:32 +0800

    whitespace adjustments

--- psmdlsyncer/models/Teacher.py
+++ psmdlsyncer/models/Teacher.py
@@ -3,14 +3,9 @@
 from psmdlsyncer.models.Entry import Entry
 import re
 from psmdlsyncer.utils.Utilities import no_whitespace_all_lower, derive_departments
-
 PRIMARYSCHOOLID = 111
 SECONDARYSCHOOLID = 112
-
-# self.is_secondary
-
 class Teacher(Entry):
-
    def __init__(self, lastfirst, num, email, title, schoolid, **kwargs):
        self.num = num
        self.idnumber = self.num
@@ -40,26 +35,20 @@ class Teacher(Entry):
        if schoolid == str(SECONDARYSCHOOLID):
            self.is_secondary = True
            self.profile_bool_issecteacher = True
-
        self.profile_extra_isteacher = True
-
    def update_courses(self, course_obj):
        course_name = course_obj.moodle_short
        if course_name not in self._courses:
            if course_name.startswith('HROOM'):
                self.homeroom = int(re.sub('[A-Z]', '', course_name.upper()))
            self._courses.append(course_name)
-
    def update_students(self, s):
        if s not in self._students:
            self._students.append(s.username)
-
    def courses(self):
        return self._courses
-
    def students(self):
        return self._students
-
    def derive_cohorts(self):
        """ Returns cohorts, dynamically created """
        l = self.get_departments()
@@ -70,15 +59,10 @@ class Teacher(Entry):
        if self.is_secondary or self.is_primary:
            l.append('teachersALL')
        return l
-
    def get_departments(self):
        self._departments = derive_departments([c for c in self.courses()])
        return self._departments
-
    def __repr__(self):
        return self.format_string("{first}{preferred_name}:{username}{mid}{courses_str}", first="+ ", mid="\n| ", last="| ", courses_str=", ".join([a for a in self._courses if a]))
-
-
 if __name__ == "__main__":
-
    pass
