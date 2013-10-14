b73c05018c78f59686695e38a9232a103e2b03d3
Adam Morris <amorris@mistermorris.com>
2013-10-14 09:56:06 +0800

    notices moved to right place

---
+++ psmdlsyncer/notices/StudentNotices.py
@@ -0,0 +1,95 @@
+#!/usr/local/bin/python3
+    
+from psmdlsyncer.settings import define_command_line_arguments
+from psmdlsyncer.html_email.Email import Email
+from psmdlsyncer.utils.Dates import custom_strftime, today
+from DatabaseBase import ExtendMoodleDatabaseToAutoEmailer
+from Model import DatabaseObject
+from Samples import student_notices_samples, student_notices_tag_samples
+import datetime
+class Nothing(Exception): pass
+
+k_record_id = 2
+
+class Student_Notices(ExtendMoodleDatabaseToAutoEmailer):
+    """
+    Converts a database on moodle into a useable system that emails users
+    """
+    def __init__(self):
+        self.verbose = False
+        self.end_of_item = ""
+        super().__init__('Secondary Notices Database')
+        self.settings = define_command_line_arguments('email', 'wordpress',
+                                                      *self.shared_command_line_args_switches,
+                                                      **self.shared_command_line_args_strings)
+        self.init()
+        #self.start_html_tag = '<html><p><i>Translations available: <a href="http://sites.ssis-suzhou.net/ssakorean/">Korean</a></i></p>'
+
+    def define(self):
+        """
+        Called by init
+        """
+        super().define()
+        self.sender = 'DragonNet Admin <lcssisadmin@student.ssis-suzhou.net>'
+        self.agents = ['Peter Fowles <peterfowles@ssis-suzhou.net>']
+        self.agent_map = {}
+
+        self.search_date = "next day"
+
+        self.content_field = 'Full Content'
+        self.attachment_field = 'Attached Content'
+        self.section_field = 'School Section'
+
+        self.priority_ids = [32]
+
+    def samples(self):
+        return student_notices_samples
+
+    def section_samples(self):
+        return student_notices_tag_samples
+
+    def email_to_agents(self):
+        if self.agents:
+            self.verbose and print("Sending notices to {}".format(self.agents))
+            message_to_staff = """<p>Edit these notices by <a href="http://dragonnet.ssis-suzhou.net/mod/data/view.php?d=5">going here</a>.</p>"""
+            self.format_for_email()
+            if self.settings.no_emails:
+                print("NOT sending email... but this is what would have been sent:")
+                self.print_email(self.agents)
+            else:
+                self.verbose and print(self.get_subject())
+                self.verbose and print(self.get_html(first_p_block=message_to_staff))
+                self.email(self.agents)
+
+        if self.agent_map:
+            raise NotImplemented
+
+    def get_subject(self, just_date=False):
+        if just_date:
+            return self.subject_output[20:]
+        else:
+            return self.subject_output
+
+    def get_html(self, first_p_block=""):
+        d = {
+            'first_p_block':first_p_block
+            }
+        return self.html_output.format(**d)
+
+    def tag_not_found(self, tag):
+        """ What to do? """
+        pass
+
+
+
+if __name__ == "__main__":
+    notices = Student_Notices()
+    # TURN ON THE ABILITY TO CLICK "EDIT" NEXT TO EACH ONE.
+    # THIS REQUIRES THAN A dbid FIELD BE CREATED ON THE SERVER SIDE
+    # TODO: STREAMLINE THIS BETTER
+    if notices.settings.email:
+        notices.email_editing = True
+        notices.email_to_agents()
+    if notices.settings.wordpress:
+        notices.email_editing = False
+        notices.post_to_wordpress('secondarystudentannouncements', datetime.time(hour=19,minute=5,second=0), date=today())
---
+++ psmdlsyncer/notices/TeacherNotices.py
@@ -0,0 +1,83 @@
+from DatabaseBase import ExtendMoodleDatabaseToAutoEmailer
+import re
+from Samples import teacher_notices_samples, teacher_notices_tag_samples
+from psmdlsyncer.settings import define_command_line_arguments
+
+class Nothing(Exception): pass
+
+# Following gives me 1st, 2nd, 3rd, 4th, etc
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
+class Teacher_Notices(ExtendMoodleDatabaseToAutoEmailer):
+    """
+    Converts a database on moodle into a useable system that emails users
+    """
+
+    def __init__(self):
+        super().__init__('Teacher Notices Database')
+        self.settings = define_command_line_arguments('group_sec_all',
+                                                      *self.shared_command_line_args_switches,
+                                                      **self.shared_command_line_args_strings)
+        self.init()
+        self.start_html_tag = '<html><p><i>Teacher Notices are now published at 7:00 pm the day before. They are edited at 5:00 pm. <a href="http://sites.ssis-suzhou.net/secondarystudentannouncements/">Click here Secondary Student Notices</i></a></p>'
+
+    def define(self):
+        """
+        Called by init
+        """
+        super().define()
+        self.sender = 'DragonNet Admin <lcssisadmin@student.ssis-suzhou.net>'
+        self.agents = ['adammorris@ssis-suzhou.net']
+        self.agent_map = {
+            'rebeccalouiseclentwo@ssis-suzhou.net':['Whole School', 'Elementary'],
+            'nicholaslittle@ssis-suzhou.net':['Whole School', 'Secondary']
+            }
+        self.search_date = "next day"
+        self.content_field = 'Full Content'
+        self.attachment_field = 'Attached Content'
+        self.section_field = 'School Section'
+        self.priority_ids      = [32, 14, 48]
+
+    def post_to_wordpress(self):
+        """ Teacher notices doesn't have a wordpress site ...  yet? """
+        pass
+
+    def get_subject(self, just_date=False):
+        if just_date:
+            return self.subject_output[20:]
+        else:
+            return self.subject_output
+
+    def get_html(self, first_p_block=""):
+        d = {
+            'first_p_block':first_p_block
+            }
+        return self.html_output.format(**d)
+
+    def section_not_found(self, tag):
+        """ What to do? """
+        pass
+
+    def samples(self):
+        return teacher_notices_samples
+
+    def section_samples(self):
+        return teacher_notices_tag_samples
+
+
+if __name__ == "__main__":
+    try:
+        notices = Teacher_Notices()
+        notices.email_editing = True
+        if notices.settings.group_sec_all:  #TODO: CHANGE em_only TO SOMETHING MORE SENSIBLE
+            notices.email_editing = False
+            notices.agent_map = {
+                'group-sec-all@ssis-suzhou.net':['Whole School', 'Secondary']
+            }
+        notices.email_to_agents()
+    except Nothing:
+        print("No matching entries found")
