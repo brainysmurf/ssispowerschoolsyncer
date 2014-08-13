2fb5de4d0a02d6df42833b721fa538575c1e3bbf
Adam Morris <amorris@mistermorris.com>
2014-08-13 21:36:41 +0800

    added templates

---
+++ psmdlsyncer/syncing/templates/__init__.py
@@ -0,0 +1,5 @@
+from psmdlsyncer.syncing.templates.default import DefaultTemplate
+from psmdlsyncer.syncing.templates.moodle import MoodleTemplate
+from psmdlsyncer.syncing.templates.enrollment import EnrollmentsOnlyTemplate
+
+__all__ = [DefaultTemplate, MoodleTemplate, EnrollmentsOnlyTemplate]
