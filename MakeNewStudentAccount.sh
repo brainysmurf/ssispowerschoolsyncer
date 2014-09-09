7db61a6a96f8400982501a5c8e37247fedb54b8e
Adam Morris <amorris@mistermorris.com>
2014-09-09 20:35:21 +0800

    make new account mod

--- MakeNewStudentAccount.sh
+++ MakeNewStudentAccount.sh
@@ -12,12 +12,3 @@ fullname=$3
 echo "$2: $1"
 echo "- - - - - - - - - - - - -"
 echo "$2"":""$defaultpassword"":::""$1"":/home/$2:/bin/bash" | $newuserspath
-
-mailfile="/var/lib/squirrelmail/data/$username"".pref"
-echo "Adding static webmail preferences to $mailfile"
-touch $mailfile
-chown www-data:www-data $mailfile
-cat /var/lib/squirrelmail/data/default_pref > $mailfile
-echo "Adding dynamic webmail preferences"
-echo "full_name=$fullname" >> $mailfile
-echo "-------------------------"
